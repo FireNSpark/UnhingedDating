05f82d18130cfc9d23ed9e48aff55ce410737d71
FireNSpark <unfavorablejosh@gmail.com>
2025-08-13 05:08:25 -0400

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -31,7 +31,7 @@ async function doSignIn(){
     await signInWithEmailAndPassword(auth, em, pw);
     msg('Signed in!', true);
     // redirect after sign-in
-    location.replace('./profile.html');
+    location.replace('./home.html');
   }catch(err){
     const m = err?.message || String(err);
     msg(m);
