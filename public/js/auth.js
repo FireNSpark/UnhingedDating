542dc0abcff3aedb5a6ccdccb7687e0c4fbe4aa0
FireNSpark <unfavorablejosh@gmail.com>
2025-08-12 20:06:02 -0400

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,29 +1,23 @@
-<script type="module">
-  import { auth } from "./firebase.js";
-  import {
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    updateProfile,
-    onAuthStateChanged,
-    signOut,
-  } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+// /public/js/auth.js  (pure ES module)
+import { auth } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
-  // Sign up
-  export async function doSignup(name, email, pass) {
-    const { user } = await createUserWithEmailAndPassword(auth, email, pass);
-    if (name) await updateProfile(user, { displayName: name });
-    return user;
-  }
+export async function doSignup(name, email, pass) {
+  const { user } = await createUserWithEmailAndPassword(auth, email, pass);
+  if (name) await updateProfile(user, { displayName: name });
+  return user;
+}
 
-  // Login
-  export async function doLogin(email, pass) {
-    const { user } = await signInWithEmailAndPassword(auth, email, pass);
-    return user;
-  }
+export async function doLogin(email, pass) {
+  const { user } = await signInWithEmailAndPassword(auth, email, pass);
+  return user;
+}
 
-  // Logout
-  export function doLogout(){ return signOut(auth); }
-
-  // Auth watcher (use on protected pages)
-  export function onUser(cb){ return onAuthStateChanged(auth, cb); }
-</script>
+export function doLogout(){ return signOut(auth); }
+export function onUser(cb){ return onAuthStateChanged(auth, cb); }
